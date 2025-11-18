7c933b8ff769c335ec3e5ed68a93d8b8f016a415
BACK-0209 <back-0209@Erxess-Mac-mini.local>
2025-11-18 04:37:27 -0800

    css

---
+++ src/controllers/user.js
@@ -0,0 +1,17 @@
+import e from "express";
+
+export const login = (req, res) => {
+  const email =req.body.email
+  const password =req.body.password
+
+
+  // if(email  && password){
+
+  // res.send("Success");
+
+  // }else{
+  //     res.send("asdasdasd");
+
+  // }
+
+};
