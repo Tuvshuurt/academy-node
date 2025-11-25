4f6e82855788c2797af9cc0c67e88cb77c7b3623
BACK-0209 <back-0209@Erxess-Mac-mini.local>
2025-11-25 20:48:26 +0800

    test

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,12 +1,36 @@
-export const login = (req, res) => {
+
+const getUsers = async () => {
+  const userRawData = await fs.readFile("users.json", "utf-8");
+
+  const users = JSON.parse(userRawData);
+
+  return users;
+};
+export const login = async (req, res) => {
 const {email,password}=req.body
-  res.cookie("user", "userId123", {
-    httpOnly: true,
-    secure: false
+
+
+  const users =    [{ "email": "admin@gmail.com", "password": "123", "balance": 4000 }];
+
+ const user = users.find(value => {
+    return value.email === email && value.password === password;
   });
-  res.json({
-    user: "userId123"
-  });
+
+  if (!user) {
+    
+        res.send(
+            "email eswel password buruu bn!"
+    )
+  } else {
+    res.cookie("user", email, {
+        httpOnly: true,
+        secure: false
+    });
+    res.json({
+        user: email
+    });
+  }
+ 
 };
 
 export const logout = (req, res) => {
